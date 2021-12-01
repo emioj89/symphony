ec041694c53ee07f4a0641c433ec4f8d5e9abe3c
Emiliano Ostellino <73698038+emioj89@users.noreply.github.com>
2021-12-01 12:26:50 +0100

    emi

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,20 +15,51 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
-/**
- * Undocumented function
- *
- * @return Response
- *
- * @Route("/", name="index")
- */
- public function index(): Response
+    /**
+     * Undocumented function
+     *
+     * @return Response
+     *
+     * @Route("/", name="index")
+     */
+    public function index(): Response
 
     {
         $categorys = $this->getDoctrine()
-        ->getRepository(Category::class)
-        ->findAll();
+            ->getRepository(Category::class)
+            ->findAll();
 
         return $this->render('category/index.html.twig', ['categorys' => $categorys]);
+    }
+
+
+    /**
+     * Undocumented function
+     *
+     * @param string $categoryName
+     * @return Response
+     *
+     * @Route("/{categoryName}",  methods = "get" , requirements = {"categoryName" = "[^/]+"}, name = "show")
+     */
+    public function show(string $categoryName): Response
+    {
+        $category = $this->getDoctrine()
+        ->getRepository(Category::class)
+            ->findBy(['name' => $categoryName]);
+
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'Aucune catégorie nommée ' . $categoryName
+            );
+        }
+
+        $program = $this->getDoctrine()
+        ->getRepository(Program::class)
+        ->findBy(['category' => $category[0]->getId()], ['id'=>'DESC'], 3 ,0);
+
+
+        return $this->render('category/show.html.twig', [
+            'categoryName' => $categoryName, 'program' => $program
+        ]);
     }
 }
